31f3023c24cf32f309036635cd03289ad06edeb3
soaqa <soaqa57@gmail.com>
2023-06-30 10:09:15 +0300

    auth

--- api/views.py
+++ api/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 
-# Create your views here.
+from api.models import ApiUser, Hotel, Room, Booking
+from api.serializers import UserSerializer, HotelSerializer, RoomSerializer, BookingSerializer
+
+
+class UserModelViewSet(viewsets.ModelViewSet):
+    queryset = ApiUser.objects.all()
+    http_method_names = ['post', 'path', 'get']
+    serializer_class = UserSerializer
+
+    authentication_classes = []
+    permission_classes = []
+
+
+class HotelModelViewSet(viewsets.ModelViewSet):
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
+
+    @action(detail=True)
+    def rooms(self, request, pk=None):
+        hotel = get_object_or_404(Hotel.objects.all(), id=pk)
+        free_rooms = hotel.rooms.filter(bookings__isnull=True)
+        return Response(RoomSerializer(free_rooms, many=True).data)
+
+
+class RoomModelViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class BookingModelViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
